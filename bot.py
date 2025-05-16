aa05fca665e9c015672bd86f05d00699081fe4e0
SandwichCode <kaspars.miltins@gmail.com>
2025-05-16 15:06:25 +0300

    Initial commit

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True  # Only needed if your bot uses member info
+
 import discord
 from discord.ext import commands
 from discord.ui import View, Button, Modal, TextInput
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True  # Only needed if your bot uses member info
 
 intents = discord.Intents.default()
 intents.message_content = True
